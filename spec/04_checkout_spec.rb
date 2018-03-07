9e3d68b921e182922b2d0624ea9ccf6d0c6b266a
abdul_razzak_alke <abdul.razzak@sla-apac.com>
2018-03-07 17:30:39 +0800

    Break the test subject to multiple examples

--- spec/04_checkout_spec.rb
+++ spec/04_checkout_spec.rb
@@ -1,24 +1,25 @@
 require 'spec_helper'
 describe 'checkout service' do
 
-  before(:each) do
+  before(:all) do
     @checkout_service = CheckoutService.new
+    @checkout_service.delete_all_carts
     product_service = ProductService.new
     @classic_product = product_service.get_products_by(sku: 'classic')[0]
     @standout_product = product_service.get_products_by(sku: 'standout')[0]
     @premium_product = product_service.get_products_by(sku: 'premium')[0]
   end
 
-  it 'should add items to cart' do
-    @checkout_service.delete_all_carts
-
+  it 'should add 3 items to customer 0000 cart' do
     cart = @checkout_service.add_item(0000, @classic_product['sku'], @classic_product['price'])
     expect(cart['items'].length).to eq(1)
     cart = @checkout_service.add_item(0000, @standout_product['sku'], @standout_product['price'])
     expect(cart['items'].length).to eq(2)
     cart = @checkout_service.add_item(0000, @premium_product['sku'], @premium_product['price'])
     expect(cart['items'].length).to eq(3)
+  end
 
+  it 'should add 4 items to customer 1111 cart' do
     cart = @checkout_service.add_item(1111, @classic_product['sku'], @classic_product['price'])
     expect(cart['items'].length).to eq(1)
     cart = @checkout_service.add_item(1111, @classic_product['sku'], @classic_product['price'])
@@ -27,7 +28,9 @@ describe 'checkout service' do
     expect(cart['items'].length).to eq(3)
     cart = @checkout_service.add_item(1111, @premium_product['sku'], @premium_product['price'])
     expect(cart['items'].length).to eq(4)
+  end
 
+  it 'should add 4 items to customer 2222 cart' do
     cart = @checkout_service.add_item(2222, @standout_product['sku'], @standout_product['price'])
     expect(cart['items'].length).to eq(1)
     cart = @checkout_service.add_item(2222, @standout_product['sku'], @standout_product['price'])
@@ -36,7 +39,9 @@ describe 'checkout service' do
     expect(cart['items'].length).to eq(3)
     cart = @checkout_service.add_item(2222, @premium_product['sku'], @premium_product['price'])
     expect(cart['items'].length).to eq(4)
+  end
 
+  it 'should add 4 items to customer 3333 cart' do
     cart = @checkout_service.add_item(3333, @premium_product['sku'], @premium_product['price'])
     expect(cart['items'].length).to eq(1)
     cart = @checkout_service.add_item(3333, @premium_product['sku'], @premium_product['price'])
@@ -47,22 +52,28 @@ describe 'checkout service' do
     expect(cart['items'].length).to eq(4)
   end
 
-  it 'should fetch valid offers for the current items in the cart and apply first one' do
+  it 'should not fetch any valid offers for customer 0000 and cart total after discount 987.97' do
     offers = @checkout_service.check_for_offers(0000)
     expect(offers.length).to eq(0)
     cart = @checkout_service.apply_offer(0000, nil)
     expect(cart['total']-cart['total_discount']).to eq(987.97)
+  end
 
+  it 'should fetch first valid offer for customer 1111 and apply it so cart total after discount is 934.97' do
     offers = @checkout_service.check_for_offers(1111)
     expect(offers.length).to eq(1)
     cart = @checkout_service.apply_offer(1111, offers[0]['id'])
     expect(cart['total']-cart['total_discount']).to eq(934.97)
+  end
 
+  it 'should fetch first valid offer for customer 2222 and apply it so cart total after discount is 1294.96' do
     offers = @checkout_service.check_for_offers(2222)
     expect(offers.length).to eq(1)
     cart = @checkout_service.apply_offer(2222, offers[0]['id'])
     expect(cart['total']-cart['total_discount']).to eq(1294.96)
+  end
 
+  it 'should fetch first valid offer for customer 3333 and apply it so cart total after discount is 1519.96' do
     offers = @checkout_service.check_for_offers(3333)
     expect(offers.length).to eq(1)
     cart = @checkout_service.apply_offer(3333, offers[0]['id'])
